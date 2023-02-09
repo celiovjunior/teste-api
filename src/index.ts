59728eff41f40c34375afce465547436c04fab8c
Célio V. Júnior <celio_cjr@hotmail.com>
2023-02-09 16:49:15 -0300

    dev: fetch data from the mockend using https core module

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,28 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import https from "https";
 
-const app = express()
+https.get("https://mockend.com/juunegreiros/BE-test-api/users", (response) => {
+  var data = '';
 
-app.get("/", (req: Request, res: Response) => {
-  return res.json({ message: 'hellooo' })
+  response.on('data', (chunk) => {
+    data += chunk
+  })
+
+  response.on('end', () => {
+    console.log(JSON.parse(data))
+  })
+}).on("error", (err) => {
+  console.log("Error: " + err.message)
 })
 
-app.listen(3333)
+const app = express()
+// app.use(express.json())
+
+
+// app.get("/", (req: Request, res: Response) => {
+//   return res.json({ message: 'hellooo' })
+// })
+
+app.listen(3333, () => {
+  console.log('🐎 server is running')
+})
