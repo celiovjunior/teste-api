d7f35905676d4ff564c35512663ccb0d60842b9c
Célio V. Júnior <celio_cjr@hotmail.com>
2023-02-09 21:58:00 -0300

    dev: created a service function to fetch the data from the mockend

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,43 @@
 import express from "express";
 import https from "https";
 
-const baseURL = "https://mockend.com/juunegreiros/BE-test-api/"
+const baseURL = "https://mockend.com/juunegreiros/BE-test-api"
 
-https.get(`${baseURL}users`, (response) => {
-  var data = '';
 
-  response.on('data', (chunk) => {
-    data += chunk
+// colocar em outro arquivo \/
+const getData = (path) => {
+  https.get(`${baseURL}/${path}`, (response) => {
+    var data = '';
+  
+    response.on('data', (chunk) => {
+      data += chunk
+    })
+  
+    response.on('end', () => {
+      JSON.parse(data)   
+    })
+  }).on("error", (err) => {
+    console.log("Error: " + err.message)
   })
-
-  response.on('end', () => {
-    console.log(JSON.parse(data))      
-  })
-}).on("error", (err) => {
-  console.log("Error: " + err.message)
-})
-
-https.get(`${baseURL}products`, (response) => {
-  var productData = '';
-
-  response.on('data', (chunk) => {
-    productData += chunk
-  })
-
-  response.on('end', () => {
-    // console.log(JSON.parse(productData))
-  })
-}).on("error", (err) => {
-  console.log("Error: " + err.message)
-})
+}
 
 const app = express()
 app.use(express.json())
 
 app.get('/users', (req, res) => {
-  return res.send({"message": "hello users endpoint"})
+  
+  const usersData = getData("users")
+  return res.send(usersData)
+  
 })
 
 app.get('/products', (req, res) => {
-  return res.send({ "message": "hello products endpoint" })
+  getData("products")
 })
+
+// app.get('/products', (req, res) => {
+//   return res.send({ "message": "hello products endpoint" })
+// })
 
 app.listen(3333, () => {
   console.log('🐎 Server is running!')
